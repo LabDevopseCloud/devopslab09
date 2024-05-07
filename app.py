7dc56bfae375922d954a159aab4aa8c969173b71
LabDevopseCloud <167546702+LabDevopseCloud@users.noreply.github.com>
2024-05-07 19:54:44 +0000

    Upgrade

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello Mariana"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return "Hello World"
